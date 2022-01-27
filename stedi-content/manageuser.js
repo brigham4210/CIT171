c785f1f9fee6c4279826ed3ee1a953c695364dc9
Brigham Chen <brigham4210@gmail.com>
2022-01-27 16:35:25 -0700

    change

--- stedi-content/manageuser.js
+++ stedi-content/manageuser.js
@@ -11,10 +11,10 @@ function setusername(){
 
 function setuserpassword(){
     password = $("#password").val();
-    var valid=passwordRegEx.exec(password);
-    if (!valid){
-        alert('Must be 6 digits, upper, lower, number, and symbol');
-    }
+    //var valid=passwordRegEx.exec(password);
+    //if (!valid){
+    //    alert('Must be 6 digits, upper, lower, number, and symbol');
+    //}
 }
 
 function setverifypassword(){
@@ -45,16 +45,29 @@ function checkexpiredtoken(token){
         dataType: 'text' })
     }
 }
+function get_code(){
+    setusername();
+    $.ajax({
+        type: 'POST',
+        url: 'https://dev.stedi.me/twofactorlogin/' + userName,
+        data: JSON.stringify({userName, password}),
+        //success: function(data) {
+        //    window.location.href = "/timer.html#"+data;//add the token to the url
+        //},
+        contentType: "application/text",
+        dataType: 'text'
+    });
 
+}
 function userlogin(){
     setuserpassword();
     setusername();
     $.ajax({
         type: 'POST',
-        url: 'https://dev.stedi.me/twofactorlogin/208-226-0144',
-        data: JSON.stringify({userName, password}),
+        url: 'https://dev.stedi.me/twofactorlogin',
+        data: JSON.stringify({phoneNumber: userName, oneTimePassword: password}),
         success: function(data) {
-            // window.location.href = "/timer.html#"+data;//add the token to the url
+            window.location.href = "/timer.html#"+data;//add the token to the url
         },
         contentType: "application/text",
         dataType: 'text'
